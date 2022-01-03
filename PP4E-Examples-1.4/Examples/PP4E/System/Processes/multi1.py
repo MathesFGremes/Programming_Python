8306024fdf91a5b734dc5fc7f636f359784cd53e
Matheus Felipe Gremes <matheusfelipegremes@gmail.com>
2022-01-03 10:03:34 -0300

    Testando p.join()

--- PP4E-Examples-1.4/Examples/PP4E/System/Processes/multi1.py
+++ PP4E-Examples-1.4/Examples/PP4E/System/Processes/multi1.py
@@ -7,22 +7,33 @@ starts new interpreter on windows, forks a new process on unix;
 
 import os
 from multiprocessing import Process, Lock
+import time
 
 def whoami(label, lock):
     msg = '%s: name:%s, pid:%s'
     with lock:
         print(msg % (label, __name__, os.getpid()))
 
+def whoamiSleep(label, lock):
+    msg = '%s: name:%s, pid:%s'
+    with lock:
+        print(msg % (label, __name__, os.getpid()))
+    time.sleep(3)
+
 if __name__ == '__main__':
     lock = Lock()
     whoami('function call', lock)
 
-    p = Process(target=whoami, args=('spawned child', lock))
+    p = Process(target=whoamiSleep, args=('spawned child', lock))
     p.start()
-    p.join()
+    #p.join()
+    
 
     for i in range(5):
         Process(target=whoami, args=(('run process %s' % i), lock)).start()
+    
+    #p.start()
+    #p.join()
 
     with lock:
         print('Main process exit.')
